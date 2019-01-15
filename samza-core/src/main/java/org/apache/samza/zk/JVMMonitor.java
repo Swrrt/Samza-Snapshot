932f9aef0b9f33a0161e32d8f17a0eb48bd5ebd1
unknown <workshop@soccf-snr4-004.comp.nus.edu.sg>
2019-01-15 15:15:53 +0800

    Start JMX locally

--- samza-core/src/main/java/org/apache/samza/zk/JVMMonitor.java
+++ samza-core/src/main/java/org/apache/samza/zk/JVMMonitor.java
@@ -1,29 +1,52 @@
 package org.apache.samza.zk;
 
 
+import com.sun.management.OperatingSystemMXBean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
+
 public class JVMMonitor implements Runnable{
     private static final Logger LOG = LoggerFactory.getLogger(JVMMonitor.class);
-    private JVMCPUUsage jvmcpuUsage;
     private Thread t;
     private final int MonitorSleepInterval = 1000;
+
+    private com.sun.management.OperatingSystemMXBean peOperatingSystemMXBean;
+    private java.lang.management.OperatingSystemMXBean operatingSystemMXBean;
+    private RuntimeMXBean runtimeMXBean;
+    private long previousJvmProcessCpuTime = 0;
+    private long previousJvmUptime = 0;
     JVMMonitor(){
-        jvmcpuUsage = new JVMCPUUsage();
+        peOperatingSystemMXBean = (com.sun.management.OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
+        operatingSystemMXBean = ManagementFactory.getOperatingSystemMXBean();
+        runtimeMXBean = ManagementFactory.getRuntimeMXBean();
+    }
+    private float getJvmCpuUsage() {
+        // elapsed process time is in nanoseconds
+        long elapsedProcessCpuTime = peOperatingSystemMXBean.getProcessCpuTime() - previousJvmProcessCpuTime;
+        // elapsed uptime is in milliseconds
+        long elapsedJvmUptime = runtimeMXBean.getUptime() - previousJvmUptime;
+
+        // total jvm uptime on all the available processors
+        long totalElapsedJvmUptime = elapsedJvmUptime * operatingSystemMXBean.getAvailableProcessors();
+
+        // calculate cpu usage as a percentage value
+        // to convert nanoseconds to milliseconds divide it by 1000000 and to get a percentage multiply it by 100
+        float cpuUsage = elapsedProcessCpuTime / (totalElapsedJvmUptime * 10000F);
+
+        // set old timestamp values
+        previousJvmProcessCpuTime = peOperatingSystemMXBean.getProcessCpuTime();
+        previousJvmUptime = runtimeMXBean.getUptime();
+
+        return cpuUsage;
     }
     public void run(){
         LOG.info("Running JVM CPU and memory monitor");
-
-        try{
-            jvmcpuUsage.openMBeanServerConnection();
-            jvmcpuUsage.getMXBeanProxyConnections();
-        }catch(Exception e){
-            LOG.info("Exception when connecting to MBean server: "+e.toString());
-        }
         try{
             while(true){
-                Float i = jvmcpuUsage.getJvmCpuUsage();
+                Float i = getJvmCpuUsage();
                 LOG.info("JVM CPU usage is: "+i.toString());
                 Thread.sleep(MonitorSleepInterval);
             }
