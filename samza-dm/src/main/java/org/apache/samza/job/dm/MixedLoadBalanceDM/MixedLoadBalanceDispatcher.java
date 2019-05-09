de5133f3828a135ee72d8674d2a7e8ccc94be9c4
unknown <workshop@soccf-snr4-004.comp.nus.edu.sg>
2019-05-09 14:17:02 +0800

    Debugging

--- samza-dm/src/main/java/org/apache/samza/job/dm/MixedLoadBalanceDM/MixedLoadBalanceDispatcher.java
+++ samza-dm/src/main/java/org/apache/samza/job/dm/MixedLoadBalanceDM/MixedLoadBalanceDispatcher.java
@@ -99,6 +99,10 @@ public class MixedLoadBalanceDispatcher implements DMDispatcher {
     @Override
     public void submitApplication(Allocation allocation) {
         writeLog("dispatcher submit application");
+        //
+        writeLog("Write coordinate stream");
+
+        //
         String stageId = allocation.getStageID();
         EnforcerFactory enfFac = getEnforcerFactory(stageId);
         Enforcer enf = enfFac.getEnforcer(config);
