97bef6fe40da266858a576bc1a45616c526ff1dd
unknown <workshop@soccf-snr4-004.comp.nus.edu.sg>
2019-05-25 22:22:55 +0800

    Debugging

--- samza-dm/src/main/java/org/apache/samza/job/dm/MixedLoadBalanceDM/JMXclient.java
+++ samza-dm/src/main/java/org/apache/samza/job/dm/MixedLoadBalanceDM/JMXclient.java
@@ -7,7 +7,7 @@ import org.jolokia.client.request.J4pReadResponse;
 
 public class JMXclient {
     public static  void main(String[] args)throws Exception{
-        String url = "jconsole service:jmx:rmi://192.168.0.16:41734/jndi/rmi://192.168.0.16:41853/jmxrmi";
+        String url = args[0];
         String taskId = "";
         J4pClient j4pClient = new J4pClient(url);
         J4pReadRequest req = new J4pReadRequest("org.apache.samza.container.TaskInstanceMetrics", "");
